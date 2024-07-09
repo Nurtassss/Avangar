748afa19d2c3b50bdd878547fba7c7c8e02c00a2
Зулхарнаев Нуртас Кайратулы <171017014@step.kz>
2024-07-09 19:44:52 +0500

    Initial comment2

---
+++ task-1.py
@@ -0,0 +1,10 @@
+class Cat:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+def info(self):
+        print(f" I am a dog. My name is{self.name}. i am {self.age} years old.")
+
+def make_sound(self):
+        print("Bark") 
